852e89f437c08fc62aecb3f9edd52c195086d012
quannguyen4703 <programmerquannguyen@gmail.com>
2025-06-05 21:49:07 +0700

    Update app

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
-from app import create_app
 import os
+from flask import Flask
+from config.config import config
 
-app = create_app(os.getenv('FLASK_CONFIG', 'default'))
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app.config.from_object(config['default'])
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app(os.getenv('FLASK_CONFIG', 'default'))
     app.run(host='0.0.0.0', port=5001, debug=True)
